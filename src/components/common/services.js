1319c9fc00ac97f4f4976471a2b932346184f12a
Hoonio <hoonio@hoonio.com>
2017-04-04 23:03:10 +0100

    service functions for rest api comms

---
+++ src/components/common/services.js
@@ -0,0 +1,38 @@
+let mockDB = [{id: 0, created_date: Date.now()}];
+
+const services = {
+  list: function() {
+    if (localStorage.ideas) {
+      mockDB = localStorage.ideas;
+    }
+    return mockDB;
+  },
+  create: function() {
+    mockDB.push({
+      id: mockDB.length,
+      created_date: Date.now()
+    });
+    console.log(mockDB);
+    return mockDB[mockDB.length];
+  },
+  update: function(idea) {
+    mockDB.find(item => item.id === idea.id).map(updateItem => {
+      updateItem.id,
+      updateItem.created_date,
+      idea.title,
+      idea.body
+    });
+    this.sync();
+    return idea;
+  },
+  delete: function(idea) {
+    mockDB = mockDB.map(item => item.id != idea.id);
+    this.sync();
+    return {id: idea.id};
+  },
+  sync: function() {
+    localStorage.setItem('ideas', mockDB);
+  }
+}
+
+export default services;
